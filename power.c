8489173fc4a368675691996edcf0941ccc0bc80c
NeelamSE <122192725+NeelamSE@users.noreply.github.com>
2023-01-14 19:18:52 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=2,b=5;
+    int c;
+    c= pow(2,3);
+    printf("2 to the power 3 is equal to %d",c);
+
+    return 0;
+}
---
+++ square_root.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    printf("enter the number you waant a square root \n");
+    scanf("%d",&a);
+    b=sqrt(a);
+    printf("square root of %d is equal to %d",a,b);
+    
+
+    return 0;
+}
